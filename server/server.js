1d3cd2ce366ff317542ef3f0d78d607c4d4a7c43
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-21 07:25:03 +0000

    resave data/schema.sql and post request in server.js

--- server/server.js
+++ server/server.js
@@ -27,13 +27,21 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
-app.post('api/album', async (req, res, next) => {
-  // try {
-  //   const {name, description} = req.body;
-  //   if (name === '') {
-  //     throw new ClientError()
-  //   }
-  // }
+app.post('/api/album', async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const sql = `
+      insert into album ("name", "description)
+      values ($1, $2)
+      returning *
+      `;
+    const params = [name, description];
+    const result = await db.query(sql, params);
+    const [newAlbum] = result.rows;
+    res.status(201).json(newAlbum);
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.use(errorMiddleware);
