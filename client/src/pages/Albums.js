32dc1ef85f97f8e26a6ac1d275a7784dee5f63cf
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-25 17:14:29 +0000

    Routing for first feature completed

--- client/src/pages/Albums.js
+++ client/src/pages/Albums.js
@@ -1,11 +1,14 @@
 import AddButton from "../components/AddButton";
 import AlbumList from "../components/AlbumList";
+import { Link } from 'react-router-dom';
 
 export default function Albums() {
 
   return (
     <>
-      <AddButton buttonName='New Album' url='/api/albums' />
+      <Link to="/api/albums/new">
+        <AddButton buttonName='New Album' url='/api/albums' />
+      </Link>
       <AlbumList />
     </>
   )
